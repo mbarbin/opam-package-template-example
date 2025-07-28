31b66f885fc105ee169182abfa072246cb95c229
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-07-28 18:24:19 +0200

    More consistent cli commands and args doc strings

--- lib/example/src/example.ml
+++ lib/example/src/example.ml
@@ -2,9 +2,9 @@ let hello_world = [%sexp "Hello, World!"]
 
 let print_cmd =
   Command.make
-    ~summary:"print hello world"
+    ~summary:"Print hello world."
     (let%map_open.Command () = Arg.return () in
      print_s hello_world)
 ;;
 
-let main = Command.group ~summary:"" [ "print", print_cmd ]
+let main = Command.group ~summary:"An example command." [ "print", print_cmd ]
